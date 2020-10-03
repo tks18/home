929df9ad59173f0b9920d0e2d4a375fc9839cbd8
Shan.tk <tksudharshan@gmail.com>
2020-10-03 20:10:43 +0530

    Pretiffy Complete Home Page; Improved Heros and Footers

---
+++ src/plugins/helpers.js
@@ -0,0 +1,22 @@
+export function scrollTo(element, scrollPixels, duration) {
+    const scrollPos = element.scrollLeft;
+    if ( !( (scrollPos === 0 || scrollPixels > 0) && (element.clientWidth + scrollPos === element.scrollWidth || scrollPixels < 0)))
+    {
+      const startTime =
+        "now" in window.performance
+          ? performance.now()
+          : new Date().getTime();
+  
+      const scroll = (timestamp) => {
+        const timeElapsed = timestamp - startTime;
+        const progress = Math.min(timeElapsed / duration, 1);
+        element.scrollLeft = scrollPos + scrollPixels * progress;
+        if (timeElapsed < duration) {
+          window.requestAnimationFrame(scroll);
+        } else {
+          return;
+        }
+      }
+      window.requestAnimationFrame(scroll);
+    }
+  }
