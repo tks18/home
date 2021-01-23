a6b3bb1e7f71a7c43ecd3e311468baaa5d8d2d5e
tks18 <tksudharshan@gmail.com>
2021-01-23 14:32:54 +0530

    Update Assets to Compressed Format

--- src/components/nav-drawer.vue
+++ src/components/nav-drawer.vue
@@ -62,8 +62,7 @@ export default {
   },
   data: function () {
     return {
-      avatar:
-        'https://i.ibb.co/hRxTz1g/c0ec2e063895760baa493c36d2d28387-s-400.jpg',
+      avatar: 'https://i.ibb.co/9YwxPwZ/IMG-20191218-222419-347.webp',
       navPaths: [
         {
           icon: 'mdi-home',
