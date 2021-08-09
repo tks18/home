0f75d2abecb94d2527b216fd6783911aa7a1dd1e
tks18 <tksudharshan@gmail.com>
2021-08-09 06:13:06 +0530

    test: revert blog component changes

--- src/views/home/components/blog-component.vue
+++ src/views/home/components/blog-component.vue
@@ -21,72 +21,6 @@
         </v-btn>
       </v-col>
     </v-row>
-    <!-- Fetching Blog Posts to be Implemented in Future -->
-    <!-- <div
-      ref="posts"
-      class="scrollable-x columns ma-2 pa-1 is-vcentered is-mobile"
-    >
-      <div
-        v-for="n in 10"
-        :key="n"
-        class="scrollable-x-child column ma-0 pa-0 non-touch point-cursor"
-      >
-        <v-hover>
-          <template #default="{ hover }">
-            <v-card
-              v-ripple
-              elevation="3"
-              class="mx-2"
-              :width="ismobile ? 225 : 250"
-              :height="ismobile ? 300 : 325"
-            >
-              <v-card>
-                <v-img
-                  src="https://i.ibb.co/BftbgSg/download-force-true-w-1920.jpg"
-                />
-              </v-card>
-              <v-card-text>
-                <h2 class="text-subtitle-1 font-weight-bold primary--text">
-                  This is a Lorem Ipsum Post
-                </h2>
-              </v-card-text>
-              <transition name="fade" mode="out-in">
-                <v-overlay
-                  v-if="hover"
-                  absolute
-                  z-index="2"
-                  opacity="0.4"
-                  color="primary"
-                  class="back-blur"
-                >
-                  <v-row class="mx-3">
-                    <v-col>
-                      <v-row align="start">
-                        <div
-                          class="text text-body-1 font-weight-bold black--text"
-                        >
-                          Overview
-                        </div>
-                      </v-row>
-                      <v-row align="start" justify="start">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. A necessitatibus, molestiae magni deleniti hic
-                        natus facilis.
-                      </v-row>
-                      <v-spacer />
-                      <v-row class="mt-1">
-                        <v-btn color="primary" small>Read More</v-btn>
-                      </v-row>
-                    </v-col>
-                  </v-row>
-                </v-overlay>
-              </transition>
-            </v-card>
-          </template>
-        </v-hover>
-      </div>
-    </div> -->
-    <!-- Temporary Skeleton  -->
     <v-row>
       <v-col align="center">
         <v-skeleton-loader type="card" max-width="300" class="mx-auto" />
@@ -95,7 +29,6 @@
         </div>
       </v-col>
     </v-row>
-    <!-- End - Temporary Skeleton  -->
   </div>
 </template>
 
