35a7e0cbbaf8d50e6b9961889002a3c3d8bd77e1
tks18 (from Travis CI) <tksudharshan@gmail.com>
2021-11-18 16:16:13 +0000

    Deploy tks18/matte-portfolio to github.com/tks18/matte-portfolio.git:gh-pages

---
+++ js/chunk-2d0b381b.8a16bb4c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b381b"],{2953:function(n,u,l){"use strict";l.r(u);var c,e,o=l("2877"),p={},s=Object(o["a"])(p,c,e,!1,null,null,null);u["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0b381b.8a16bb4c.js.map
