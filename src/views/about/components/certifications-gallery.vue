2a5da3395cd9667683ed0fd2a771f8f83259c6bb
tks18 <tksudharshan@gmail.com>
2022-03-20 12:32:15 +0530

    feat(routes/about): use tabs instead of sliders for certification gallery

--- src/views/about/components/certifications-gallery.vue
+++ src/views/about/components/certifications-gallery.vue
@@ -9,98 +9,109 @@
     <v-card-subtitle> {{ subtitleQuote }} </v-card-subtitle>
     <v-card-text>
       <v-row v-if="!loading">
-        <v-col cols="12">
-          <v-row>
-            <v-col :cols="ismobile ? 12 : 7">
-              <v-img :src="currentCertificate.image" />
-            </v-col>
-            <v-col :cols="ismobile ? 12 : 5">
-              <v-row align="center">
-                <v-col cols="12">
-                  <div class="text-h6 font-weight-black">
-                    {{ currentCertificate.details.title }}
-                  </div>
-                  <div class="text-body-1">
-                    {{ currentCertificate.details.description }}
-                  </div>
-                  <v-hover v-slot="{ hover }">
-                    <div
-                      class="text-overline point-cursor fit-text"
-                      @click="gotoUrl(currentCertificate.organisation.link)"
-                    >
-                      issued by
-                      <span
-                        :class="
-                          'font-weight-black ' + (hover ? 'primary--text' : '')
-                        "
-                      >
-                        {{ currentCertificate.organisation.name }}
-                      </span>
-                      on
-                      <span class="font-weight-black primary--text">
-                        {{ currentCertificate.details.issued_on }}
-                      </span>
-                    </div>
-                  </v-hover>
-                  <v-btn
-                    rounded
-                    outlined
-                    color="primary"
-                    @click="gotoUrl(currentCertificate.verifylink)"
-                  >
-                    Verify this Certificate
-                  </v-btn>
+        <v-tabs-items v-model="currentCertificate">
+          <v-tab-item
+            v-for="(certificate, index) in certifications"
+            :key="index"
+          >
+            <v-col cols="12">
+              <v-row>
+                <v-col :cols="ismobile ? 12 : 5">
+                  <v-img
+                    v-ripple
+                    dark
+                    :src="certificate.image"
+                    :aspect-ratio="4 / 3"
+                    contain
+                    class="point-cursor"
+                    @click="gotoUrl(certificate.verifylink)"
+                  />
                 </v-col>
-                <v-spacer />
-                <v-col cols="12">
-                  <div class="text-body-1 font-weight-black my-2">
-                    About {{ currentCertificate.organisation.name }}
-                    <div class="text-body-2">
-                      {{ currentCertificate.organisation.about }}
-                    </div>
-                  </div>
-                  <div class="text-body-1 font-weight-black my-2">
-                    Skills Developed with this Certification
-                    <div class="text-body-2">
-                      <v-chip-group column>
-                        <v-chip
-                          v-for="(skill, index) in currentCertificate.details
-                            .skills"
-                          :key="index"
-                          outlined
-                          color="primary"
-                          @click="gotoUrl(skill.link)"
+                <v-col :cols="ismobile ? 12 : 7">
+                  <v-row align="center">
+                    <v-col cols="12">
+                      <div class="text-h6 font-weight-black">
+                        {{ certificate.details.title }}
+                      </div>
+                      <div class="text-body-1">
+                        {{ certificate.details.description }}
+                      </div>
+                      <v-hover v-slot="{ hover }">
+                        <div
+                          class="text-overline point-cursor fit-text"
+                          @click="gotoUrl(certificate.organisation.link)"
                         >
-                          {{ skill.name }}
-                        </v-chip>
-                      </v-chip-group>
-                    </div>
-                  </div>
+                          issued by
+                          <span
+                            :class="
+                              'font-weight-black ' +
+                              (hover ? 'primary--text' : '')
+                            "
+                          >
+                            {{ certificate.organisation.name }}
+                          </span>
+                          on
+                          <span class="font-weight-black primary--text">
+                            {{ certificate.details.issued_on }}
+                          </span>
+                        </div>
+                      </v-hover>
+                      <v-btn
+                        rounded
+                        outlined
+                        color="primary"
+                        @click="gotoUrl(certificate.verifylink)"
+                      >
+                        Verify this Certificate
+                      </v-btn>
+                    </v-col>
+                    <v-spacer />
+                    <v-col cols="12">
+                      <div class="text-body-1 font-weight-black my-2">
+                        About {{ certificate.organisation.name }}
+                        <div class="text-body-2">
+                          {{ certificate.organisation.about }}
+                        </div>
+                      </div>
+                      <div class="text-body-1 font-weight-black my-2">
+                        Skills Developed with this Certification
+                        <div class="text-body-2">
+                          <v-chip-group column>
+                            <v-chip
+                              v-for="(skill, subIndex) in certificate.details
+                                .skills"
+                              :key="subIndex"
+                              outlined
+                              color="primary"
+                              @click="gotoUrl(skill.link)"
+                            >
+                              {{ skill.name }}
+                            </v-chip>
+                          </v-chip-group>
+                        </div>
+                      </div>
+                    </v-col>
+                  </v-row>
                 </v-col>
               </v-row>
             </v-col>
-          </v-row>
-        </v-col>
+          </v-tab-item>
+        </v-tabs-items>
         <v-col cols="12">
           <div class="font-weight-bold text-h6">More Certifications</div>
-          <v-slide-group
+          <v-tabs
             v-model="currentCertificate"
-            active-class="success"
-            :show-arrows="!ismobile"
+            centered
+            center-active
+            icons-and-text
+            :show-arrows="false"
+            grow
+            height="130"
           >
-            <v-slide-item
-              v-for="(certs, index) in certifications"
-              :key="index"
-              class="my-2 mx-2"
-            >
-              <v-card
-                :img="certs.image"
-                height="130"
-                width="260"
-                @click="toggleCert(certs)"
-              />
-            </v-slide-item>
-          </v-slide-group>
+            <v-tab v-for="(certificate, index) in certifications" :key="index">
+              <v-card :img="certificate.image" height="130" width="260" />
+            </v-tab>
+          </v-tabs>
         </v-col>
       </v-row>
       <v-row v-if="loading">
