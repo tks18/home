6f78738734b75b2133cb6456c6c2850a26d761f4
tks18 <tksudharshan@gmail.com>
2022-04-01 20:59:21 +0530

    feat(routes): introduce blog posts from hashnode through graphql API

--- src/plugins/backend/blog.js
+++ src/plugins/backend/blog.js
@@ -7,14 +7,6 @@ export default {
       url: api.blog.get,
       store,
     });
-    return resp;
-  },
-  set: async (postData, store) => {
-    const resp = await requester({
-      url: api.blog.set,
-      postData,
-      store,
-    });
-    return resp;
+    return resp.data;
   },
 };
--- src/views/home/components/blog-component.vue
+++ src/views/home/components/blog-component.vue
@@ -6,36 +6,64 @@
           id="blogtitle"
           :title="title"
           arrow="right"
-          link="/blog"
+          link="https://blog.sudharshan.tk"
+          :outside="true"
           :large="false"
           :center="false"
           :darker="false"
         />
       </v-col>
-      <v-col align="end" justify="start" class="mr-4">
-        <v-btn icon color="primary" @click="swipeLeft('posts')">
-          <v-icon>mdi-arrow-left</v-icon>
-        </v-btn>
-        <v-btn icon color="primary" @click="swipeRight('posts')">
-          <v-icon>mdi-arrow-right</v-icon>
-        </v-btn>
+    </v-row>
+    <v-row v-if="loading">
+      <v-col align="center">
+        <v-skeleton-loader
+          type="card"
+          max-width="340"
+          height="240"
+          class="mx-auto"
+        />
+        <div class="text-body-1">Loading Blog Posts</div>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col align="center">
-        <v-skeleton-loader type="card" max-width="300" class="mx-auto" />
-        <div class="text-body-1">
-          Blog Posts Under Construction, I will Come Striking
-        </div>
+    <v-row v-if="!loading" align="start" justify="start" class="mx-2">
+      <v-col
+        v-for="(post, index) in posts"
+        id="posts"
+        :key="index"
+        class="non-touch"
+        cols="3"
+        align="center"
+      >
+        <v-card
+          max-width="340"
+          @click="gotoUrl(`https://${domain}/${post.slug}`)"
+        >
+          <v-img :src="post.coverImage" aspect-ratio="16/9" height="200" />
+          <v-row align="end" justify="end">
+            <v-col align="left">
+              <v-card-title class="no-break-words">
+                {{ post.title }}
+              </v-card-title>
+              <v-card-subtitle>
+                Posted on {{ post.dateAdded | moment('DD MMMM, YYYY') }}
+              </v-card-subtitle>
+              <v-card-text v-if="index === 0">
+                <v-chip color="primary">
+                  <v-icon left>mdi-history</v-icon>
+                  New
+                </v-chip>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
-/* eslint-disable no-eval */
-import { scrollTo } from '@p/helpers';
 import title_component from '@v/home/components/common/title-component';
+import { blog } from '@p/backend';
 
 export default {
   name: 'HomeBlogComponent',
@@ -51,16 +79,30 @@ export default {
       type: Boolean,
       default: false,
     },
-  },
-  data: () => ({}),
-  methods: {
-    swipeLeft(func) {
-      const content = `this.$refs.${func}`;
-      scrollTo(eval(content), -400, 300);
+    gotoUrl: {
+      type: Function,
+      default: () => {},
     },
-    swipeRight(func) {
-      const content = `this.$refs.${func}`;
-      scrollTo(eval(content), 400, 300);
+  },
+  data: () => ({
+    loading: true,
+    posts: [],
+    domain: '',
+  }),
+  mounted() {
+    this.getBlogPosts();
+  },
+  methods: {
+    async getBlogPosts() {
+      const blogData = await blog.get(this.$state.store);
+      const sortedPosts = this.$lodash.orderBy(
+        blogData.data.user.publication.posts,
+        [(post) => new Date(post.dateAdded)],
+        ['desc'],
+      );
+      this.posts = sortedPosts;
+      this.domain = blogData.data.user.publicationDomain;
+      this.loading = false;
     },
   },
 };
