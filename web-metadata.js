4c79aed022a66edff90708c24f9bb8cb5b2e3695
tks18 <tksudharshan@gmail.com>
2022-04-02 07:22:23 +0530

    style(icons): change website logo, banner & favicons

--- web-metadata.js
+++ web-metadata.js
@@ -4,13 +4,13 @@ const { baseSite } = require('./package.json');
 const twitterData = {
   type: 'summary_large_image',
   username: '@shantk18',
-  pic: 'assets/photos/me.jpg',
+  pic: 'https://i.ibb.co/v1WvYQG/Home-Banner.png',
 };
 const keyWords =
   'Sudharshan TK, Portfolio, Shan.tk, https://sudharshan.tk, sudharshan.tk, sudharshan, Blog, Portfolio-blog';
 
 const webSiteDesc =
-  'Building Online Answers for Your Next Design Challenge. Sometimes Writes Blogs about Latest Tech Developments and Frameworks. Forgot, this is Sudharshan TK hehe xD';
+  'Organized, top performer and goal-driven individual. Skilled at using Various Programming Languages such as Python & Javascript to Leverage Finance Functions to build a competitive advantage.';
 
 module.exports = {
   title,
