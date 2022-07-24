7d0d7ccb35a626f7fd4411f69d14ec9e03bda473
tks18 <tksudharshan@gmail.com>
2022-07-24 16:27:05 +0530

    refactor(views/home): completely refactor the hero component in home page

--- src/views/home/components/hero-component.vue
+++ src/views/home/components/hero-component.vue
@@ -1,122 +1,117 @@
 <template>
-  <div id="home-mainhero" class="column is-full non-touch">
-    <div
-      :class="
-        (ismobile ? 'hero is-fullheight' : 'hero is-medium') +
-        ($vuetify.theme.dark ? ' grad-back-dark' : ' grad-back-light')
-      "
-    >
-      <div :class="'hero-body' + (ismobile ? ' ma-1 px-0 py-3' : ' ma-2 pa-8')">
-        <div class="container">
-          <v-row
-            align="center"
-            justify="center"
-            :class="!ismobile ? 'my-2' : ''"
-          >
-            <v-col cols="12" align="center" class="my-1 py-0">
-              <div
-                :class="
-                  'text text-center font-weight-bold' +
-                  (ismobile ? ' text-h4' : ' text-h2')
-                "
+  <v-card
+    id="home-mainhero"
+    outlined
+    :class="
+      'non-touch' +
+      ($vuetify.theme.dark ? ' grad-back-dark' : ' grad-back-light')
+    "
+  >
+    <v-card-text class="hero is-fullheight">
+      <div class="hero-body">
+        <v-row align="center" justify="center" :class="!ismobile ? 'my-2' : ''">
+          <v-col cols="12" align="center" class="my-1 py-0">
+            <div
+              :class="
+                'text text-center font-weight-bold' +
+                (ismobile ? ' text-h4' : ' text-h2')
+              "
+            >
+              <vue-typer
+                :key="$vuetify.theme.dark ? 0 : 1"
+                :text="taglines"
+                :class="$vuetify.theme.dark ? 'dark' : 'light'"
+                :repeat="Infinity"
+                :shuffle="true"
+                initial-action="typing"
+                :pre-type-delay="70"
+                :type-delay="70"
+                :pre-erase-delay="800"
+                :erase-delay="100"
+                erase-style="backspace"
+                :erase-on-complete="false"
+                caret-animation="smooth"
+              />
+            </div>
+          </v-col>
+          <v-col cols="12" class="my-0 py-0">
+            <div
+              :class="
+                'text font-weight-bold ma-2 pa-2 text-center' +
+                (ismobile ? ' text-h4' : ' text-h3')
+              "
+            >
+              <v-tooltip
+                v-model="toggleTooltip"
+                top
+                transition="slide-y-transition"
               >
-                <vue-typer
-                  :key="$vuetify.theme.dark ? 0 : 1"
-                  :text="taglines"
-                  :class="$vuetify.theme.dark ? 'dark' : 'light'"
-                  :repeat="Infinity"
-                  :shuffle="true"
-                  initial-action="typing"
-                  :pre-type-delay="70"
-                  :type-delay="70"
-                  :pre-erase-delay="800"
-                  :erase-delay="100"
-                  erase-style="backspace"
-                  :erase-on-complete="false"
-                  caret-animation="smooth"
-                />
-              </div>
-            </v-col>
-            <v-col cols="12" class="my-0 py-0">
-              <div
-                :class="
-                  'text font-weight-bold my-2 py-2 mx-2 px-2 text-center' +
-                  (ismobile ? ' text-h4' : ' text-h3')
-                "
-              >
-                <v-tooltip
-                  v-model="toggleTooltip"
-                  top
-                  transition="slide-y-transition"
-                >
-                  <template #activator="{ on, attrs }">
-                    <span
-                      v-bind="attrs"
-                      class="fit-text non-touch point-cursor"
-                      v-on="on"
-                      @click="loopRandEmoji()"
-                    >
-                      {{ animatedArray.randEmoji }}
-                    </span>
-                  </template>
-                  <span>Click here to Shuffle Emoji's</span>
-                </v-tooltip>
-              </div>
-            </v-col>
-            <v-col cols="12" class="my-0 py-0">
-              <div
-                :class="
-                  'text text-center font-weight-bold' +
-                  (ismobile ? ' text-body-2' : ' text-body-1')
-                "
-              >
-                I design and code beautifully simple things, and I love what I
-                do.
-              </div>
-            </v-col>
-            <v-col :cols="12" align="center" class="my-2 py-0">
-              <v-avatar :size="ismobile ? 250 : 350">
-                <v-img :src="heroImg" alt="Main Image">
-                  <template #placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular indeterminate color="primary" />
-                    </v-row>
-                  </template>
-                </v-img>
-              </v-avatar>
-            </v-col>
-            <v-col algin="center" :cols="12" class="my-2 py-0">
-              <div class="text text-center text-overline font-weight-bold">
-                <v-row align="center">
-                  <v-col
-                    v-for="(button, index) in heroButtons"
-                    :key="index"
-                    align="center"
+                <template #activator="{ on, attrs }">
+                  <span
+                    v-bind="attrs"
+                    class="fit-text non-touch point-cursor"
+                    v-on="on"
+                    @click="loopRandEmoji()"
                   >
-                    <v-btn
-                      large
-                      elevation="24"
-                      text
-                      raised
-                      outlined
-                      @click="$vuetify.goTo('#' + button.id)"
-                    >
-                      <v-icon> {{ button.icon }} </v-icon>
-                      {{ button.name }}
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </div>
-            </v-col>
-          </v-row>
-        </div>
+                    {{ animatedArray.randEmoji }}
+                  </span>
+                </template>
+                <span>Click here to Shuffle Emoji's</span>
+              </v-tooltip>
+            </div>
+          </v-col>
+          <v-col cols="12" class="my-0 py-0">
+            <div
+              :class="
+                'text text-center font-weight-bold' +
+                (ismobile ? ' text-body-2' : ' text-body-1')
+              "
+            >
+              I design and code beautifully simple things, and I love what I do.
+            </div>
+          </v-col>
+          <v-col :cols="12" align="center" class="my-2 py-0">
+            <v-avatar :size="ismobile ? 250 : 350">
+              <v-img :src="heroImg" alt="Main Image">
+                <template #placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular indeterminate color="primary" />
+                  </v-row>
+                </template>
+              </v-img>
+            </v-avatar>
+          </v-col>
+          <v-col algin="center" :cols="12">
+            <div class="text text-center text-overline font-weight-bold">
+              <v-row align="center">
+                <v-col
+                  v-for="(button, index) in heroButtons"
+                  :key="index"
+                  align="center"
+                >
+                  <v-btn
+                    large
+                    elevation="24"
+                    text
+                    raised
+                    outlined
+                    @click="$vuetify.goTo('#' + button.id)"
+                  >
+                    <v-icon> {{ button.icon }} </v-icon>
+                    {{ button.name }}
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </div>
+          </v-col>
+        </v-row>
       </div>
-    </div>
-  </div>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -137,7 +132,7 @@ export default {
     },
   },
   data: () => ({
-    heroImg: authorData.bannerImg,
+    heroImg: authorData.profilePicture,
     toggleTooltip: false,
     taglines: authorData.taglines,
     animatedArray: {
@@ -161,11 +156,6 @@ export default {
           name: 'Projects',
           icon: 'mdi-projector-screen',
           id: 'home-projtitle',
-        },
-        {
-          name: 'Contact Me',
-          icon: 'mdi-contactless-payment',
-          id: 'home-feedbacktitle',
         },
       ];
     },
